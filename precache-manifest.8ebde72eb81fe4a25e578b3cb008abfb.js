692dbf188e7e2c7ec22b6b0639729cdeaf5637f2
Mahmoud Zougheb <m.zougheb@gmail.com>
2019-09-28 00:43:10 -0400

    Updates

---
+++ precache-manifest.8ebde72eb81fe4a25e578b3cb008abfb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4e7423c6bddba5292173303735c6c69",
+    "url": "/hangman-guessing-word/index.html"
+  },
+  {
+    "revision": "887afeb0faf06531a3fd",
+    "url": "/hangman-guessing-word/static/css/main.0b2d2082.chunk.css"
+  },
+  {
+    "revision": "c6bd7f57004965216dc2",
+    "url": "/hangman-guessing-word/static/js/2.3a5e3c5b.chunk.js"
+  },
+  {
+    "revision": "887afeb0faf06531a3fd",
+    "url": "/hangman-guessing-word/static/js/main.3b792524.chunk.js"
+  },
+  {
+    "revision": "804bbcdcce5eade9d069",
+    "url": "/hangman-guessing-word/static/js/runtime-main.cb1bcd7d.js"
+  },
+  {
+    "revision": "d584fd827c80d18ee721a82e87782778",
+    "url": "/hangman-guessing-word/static/media/0.d584fd82.png"
+  },
+  {
+    "revision": "73eed85381a7229948dd912315b7940d",
+    "url": "/hangman-guessing-word/static/media/1.73eed853.png"
+  },
+  {
+    "revision": "0e949d9e0a6f5d6ae87501a8b1398747",
+    "url": "/hangman-guessing-word/static/media/2.0e949d9e.png"
+  },
+  {
+    "revision": "28889e2f8e16394f3aae7a5ff20045bb",
+    "url": "/hangman-guessing-word/static/media/3.28889e2f.png"
+  },
+  {
+    "revision": "0d98c1db6d9ebb4087bedab1aa7cd468",
+    "url": "/hangman-guessing-word/static/media/4.0d98c1db.png"
+  },
+  {
+    "revision": "656e9ecedac92c1c29dd965822233e33",
+    "url": "/hangman-guessing-word/static/media/5.656e9ece.png"
+  },
+  {
+    "revision": "932a05d32533ddf104c7e3f3874fcb7d",
+    "url": "/hangman-guessing-word/static/media/6.932a05d3.png"
+  }
+]);
